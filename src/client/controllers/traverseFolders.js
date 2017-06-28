104aceba27ae323414c7d0ada6ac53a587003046
Neil Romana <wazzupnickz@gmail.com>
2017-06-27 18:17:55 -0700

    add depth-first fn

--- src/client/controllers/traverseFolders.js
+++ src/client/controllers/traverseFolders.js
@@ -1,5 +1,23 @@
-const traverseFolders = (node) => {
+const traverseFolders = (value) => {
+  this.value = value;
+  this.children = [];
+};
 
+traverseFolders.prototype.DFSelect = (filter) => {
+  var results = [];
+
+  var finder = (node, depth) => {
+    if (filter(node.value, depth)) {
+      results.push(node.value);
+    }
+    for (let i = 0; i < node.children.length; i++) {
+      let child = node.children[i];
+      finder(child, depth + 1);
+    }
+  };
+  finder(this, 0);
+
+  return results;
 };
 
 module.exports = traverseFolders;
